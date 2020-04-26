86e64a23d406d1ab43cc96283f7c12e4d8505333
Naren Sulegai <86naren@gmail.com>
2020-04-26 21:36:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 window.addEventListener('beforeinstallprompt', e => {
   alert('Install pwa');
 });
+
+window.addEventListener('load', e => {
+  console.log(window.webkit);
+  window.webkit.messageHandlers.test.postMessage("Hello, world!");
+})
